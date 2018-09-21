7bd7e124daa3c4467e216856ed000709c388dda9
atsu666 <ito.atsu666@gmail.com>
2018-09-21 12:19:42 +0900

    bumping version

--- js/geo-picker.js
+++ js/geo-picker.js
@@ -6,7 +6,7 @@
  *   license: appleple
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 0.1.1
+ *   version: 0.1.2
  *
  * leaflet:
  *   license: BSD-2-Clause (http://opensource.org/licenses/BSD-2-Clause)
