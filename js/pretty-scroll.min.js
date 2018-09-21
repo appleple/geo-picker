6ac1dbec96be5f1f2faf1c9a478ddfcdc562d5e4
Hori Godai <ess_president@me.com>
2018-09-21 14:23:52 +0900

    bumping version

--- js/pretty-scroll.min.js
+++ js/pretty-scroll.min.js
@@ -6,7 +6,7 @@
  *   license: appleple
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 0.1.4
+ *   version: 0.1.5
  *
  * leaflet:
  *   license: BSD-2-Clause (http://opensource.org/licenses/BSD-2-Clause)
